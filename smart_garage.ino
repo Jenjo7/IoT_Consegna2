a327edf8fb240a50a8bd8141ed86961fc6fa493a
Gianni Savini <gianni.savini@studio.unibo.it>
2017-11-10 14:27:57 +0100

    Ok, creato repo, della seconda consegna

---
+++ smart_garage.ino
@@ -0,0 +1,19 @@
+/*
+ * ===================================================================
+ * 
+ * Seconda consegna di Sistemi Embedded e Internet of things
+ * Savini Gianni 731806
+ * 
+ * ===================================================================
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
