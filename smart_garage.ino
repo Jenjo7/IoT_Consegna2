6c3c3de38c918c428936cc82c359eadba7b023e0
Gianni Savini <gianni.savini@studio.unibo.it>
2017-11-10 14:38:23 +0100

    Aggiunte macro di base

--- smart_garage.ino
+++ smart_garage.ino
@@ -7,10 +7,22 @@
  * ===================================================================
  */
 
+//definite costanti delle misure utili al programma
+#define DISTMIN 0.1
+#define DISTMAX 1
+#define DISTCLOSE  0.5
+
+#define LDIST1 9
+#define LDIST2 10
+#define LR 11
+#define PROX A0
+#define TOUCH 3
+#define CLOSE 2
+#define PIR 7
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  
 }
 
 void loop() {
