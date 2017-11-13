36461a2ed28388123507dee080df3a0ddc23494c
Gianni Savini <gianni.savini@studio.unibo.it>
2017-11-13 16:25:08 +0100

    Ho spostato tutte le macro di configurazione dei sensori

---
+++ config.h
@@ -0,0 +1,12 @@
+//definite costanti delle misure utili al programma
+#define DISTMIN 0.1
+#define DISTMAX 1
+#define DISTCLOSE  0.5
+
+#define LDIST1 9
+#define LDIST2 10
+#define LR 11
+#define TOUCH 3
+#define CLOSE 2
+#define PIR 7
+
--- smart_garage.ino
+++ smart_garage.ino
@@ -6,19 +6,8 @@
  * 
  * ===================================================================
  */
+#include "config.h"
 
-//definite costanti delle misure utili al programma
-#define DISTMIN 0.1
-#define DISTMAX 1
-#define DISTCLOSE  0.5
-
-#define LDIST1 9
-#define LDIST2 10
-#define LR 11
-#define PROX A0
-#define TOUCH 3
-#define CLOSE 2
-#define PIR 7
 
 void setup() {
   Serial.begin(9600);
