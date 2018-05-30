f3299cd8431f0c383daf5b23643b05b2c8086f47
Gianni Savini <gianni.savini@studio.unibo.it>
2018-05-30 15:36:17 +0200

    Creato primo Task, il task di attesa

---
+++ Init.cpp
@@ -0,0 +1,23 @@
+#include "Init.h"
+#include "config.h"
+#include "Arduino.h"
+#include "MsgService.h"
+
+extern volatile State state;
+
+Init::Init() {
+  
+}
+
+void Init::init(int period) {
+  Task::init(period);
+}
+
+void Init::tick() {
+  if(MsgService.isAviable()) {
+    if(MsgService.receiveMsg().getContent() == "open") {
+      state = PRK;
+    }
+  }
+}
+
---
+++ Init.h
@@ -0,0 +1,15 @@
+#ifndef __INITTASK__
+#define __INITTASK__
+
+#include "Task.h"
+
+class Init: public Task {
+
+  public:
+
+    Init();
+    void init(int period);
+    void tick();
+};
+
+#endif
