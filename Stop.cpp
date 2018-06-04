76fef1da5cdc5d961c3323ce193ec35d40d2d589
Gianni Savini <gianni.savini@studio.unibo.it>
2018-06-04 11:53:02 +0200

    Lavori in corso sullo stato di stop

---
+++ Stop.cpp
@@ -0,0 +1,20 @@
+#include "Stop.h"
+#include "Arduino.h"
+#include "config.h"
+
+extern volatile State state;
+
+Stop::Stop(int ledd) {
+  
+}
+
+void Stop::init(int period) {
+  Task::init(period);
+}
+
+void Stop::tick() {
+  if(state == STP) {
+    int dist = 
+  }
+}
+
