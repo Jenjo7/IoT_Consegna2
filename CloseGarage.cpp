c152a4212a5809ee93992a4b6a2163a62cb50b44
Gianni Savini <gianni.savini@studio.unibo.it>
2018-06-04 11:48:00 +0200

    Creati file .cpp che implementano l'apertura e la chiusura del garage

---
+++ CloseGarage.cpp
@@ -0,0 +1,32 @@
+#include "CloseGarage.h"
+#include "Arduino.h"
+#include "config.h"
+
+extern volatile State state;
+int intensitys = MAXINTENSITY;
+
+CloseGarage::CloseGarage(int led) {
+  this->pin = led;
+}
+
+void CloseGarage::init(int period) {
+  Task::init(period);
+  fadeLed = new LedExt(pin, 0);
+  fadeLed->switchOn();
+}
+
+void CloseGarage::tick() {
+  if(state == CLS) {
+    if(intensitys >= 0) {
+//     fadeLed->switchOn();
+       Serial.println(intensitys == MAXINTENSITY ? "close" : "");
+       Serial.println(intensitys);
+       fadeLed->setIntensity(intensitys);
+       intensitys -= BRIGHT;
+    } else {
+      intensitys = MAXINTENSITY;
+      state = INI;
+    }
+  }
+}
+
---
+++ OpenGarage.cpp
@@ -0,0 +1,50 @@
+#include "config.h"
+#include "Arduino.h"
+#include "TimerOne.h"
+#include "OpenGarage.h"
+
+extern volatile State state;
+int intensity = 0;
+int inc = 0;
+
+
+
+OpenGarage::OpenGarage(int ledPin, int pirPin){
+  this->ledPin = ledPin;
+  this->pirPin = pirPin;
+}
+  
+void OpenGarage::init(int period){
+  Task::init(period);
+  fadeLed = new LedExt(ledPin, 0);  
+  pir = new Pir(pirPin);
+  fadeLed->switchOn();
+}
+
+void OpenGarage::tick() {
+  if(state == OPN) {
+    Serial.println(intensity == 0 ? "open" : "");
+    if(intensity <= MAXINTENSITY) {
+       fadeLed->switchOn();
+       Serial.println(intensity);
+       fadeLed->setIntensity(intensity);
+       intensity += BRIGHT;
+    } else {
+      if(inc++ <= WAITTIME) {
+        if(pir->isDetected()) {
+          state = PRK;
+        }
+      } else {
+        inc = 0;
+        intensity = 0;
+        state = CLS;
+      }
+    }
+  }
+}
+
+//void fade() {
+//  fadeLed->setIntensity(intensity);
+//  intensity += BRIGHT;
+//}
+
